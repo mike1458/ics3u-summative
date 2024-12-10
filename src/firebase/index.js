6c0f022a6248a2c912178b6e108ea6c59d89757b
mike <22270711aa@gmail.com>
2024-12-10 13:54:10 -0500

    Imported firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBQQgaqxWANvzW14pbJrwDTe1fNGJiuIoc",
+    authDomain: "summative-2b486.firebaseapp.com",
+    projectId: "summative-2b486",
+    storageBucket: "summative-2b486.firebasestorage.app",
+    messagingSenderId: "285302048576",
+    appId: "1:285302048576:web:be9b30a1e07e71b6741ca5"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
